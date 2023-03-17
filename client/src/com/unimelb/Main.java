456e585ea8cc0eef8bc1b61109405587bfebc5bd
JaeholsUni <jhollingswor@student.unimelb.edu.au>
2023-03-17 15:56:20 +1100

    Setup basic server and client with example code

--- client/src/com/unimelb/Main.java
+++ client/src/com/unimelb/Main.java
@@ -1,8 +1,56 @@
 package com.unimelb;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", 4444);
+            System.out.println("Connection established");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+
+            Scanner scanner = new Scanner(System.in);
+            String inputStr = null;
+
+            while (!(inputStr = scanner.nextLine()).equalsIgnoreCase("exit")){
+                out.write(inputStr + "\n");
+                out.flush();
+                System.out.println("Message Sent");
+
+                String recieved = in.readLine();
+                System.out.println("Message recieved " + recieved);
+            }
+
+            scanner.close();
+        }
+        catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- server/src/com/unimelb/Main.java
+++ server/src/com/unimelb/Main.java
@@ -1,8 +1,71 @@
 package com.unimelb;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	ServerSocket listeningSocket = null;
+    Socket clientSocket = null;
+
+    try {
+        listeningSocket = new ServerSocket(4444);
+        int i = 0; //Count the number of clients
+
+        while (true)
+        {
+            System.out.println("Server listening on port 4444");
+
+            clientSocket = listeningSocket.accept();
+            i++;
+
+            System.out.println("Client connection number" + i + "accepted:");
+            System.out.println("Remote Hostname: " + clientSocket.getInetAddress().getHostName());
+            System.out.println("Local Port " + clientSocket.getLocalPort());
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+
+            String clientMsg = null;
+            try {
+                while ((clientMsg = in.readLine()) != null)
+                {
+                    System.out.println("Message from client " + i + ": " + clientMsg);
+                    out.write("Server Ack " + clientMsg + "\n");
+                    out.flush();
+                    System.out.println("Response Sent");
+                }
+                System.out.println("Sever Closed the Client Connection - Recieved null");
+            }
+            catch (SocketException e)
+            {
+                System.out.println("Closed by exception");
+            }
+
+            clientSocket.close();
+        }
+    }
+    catch (IOException e)
+    {
+        e.printStackTrace();
+    }
+    finally {
+        if (listeningSocket != null) {
+            try {
+                listeningSocket.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
     }
 }
